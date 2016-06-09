2021a22b4c106eea17ef99b864cf1fa81b671e4f
domminka <dom@minka.io>
2016-06-09 11:42:35 -0500

    Adding Redux part 1

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//redux and immutable
 import {List, Map} from 'immutable';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
 
 // incuding material-ui setup
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -10,21 +15,29 @@ import Header from './Header';
 import TransferApp from '../transfer/TransferApp';
 //import Main from './Main';
 
-const transfers = List.of(
-  Map({id: 1, source: 'Tea', destination: 'Domagoj', description:'para cerveza', amount: '15'}),
-  Map({id: 2, source: 'Laura', destination: 'Domagoj',description:'para cafe', amount: '13'}),
-  Map({id: 3, source: 'Domagoj', destination: 'Tea',description:'para libro', amount: '100'}),
-  Map({id: 4, source: 'Domagoj', destination: 'Tea',description:'beso', amount: '100'})
-);
+// Redux store
+const store = createStore(reducer);
+// dispatch the SET_STATE
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    transfers: [
+      {id: 1, source: 'Tea', destination: 'Domagoj', description:'para cerveza', amount: '15'},
+      {id: 2, source: 'Laura', destination: 'Domagoj',description:'para cafe', amount: '13'},
+      {id: 3, source: 'Domagoj', destination: 'Tea',description:'para libro', amount: '100'},
+      {id: 4, source: 'Domagoj', destination: 'Tea',description:'beso', amount: '100'}
+    ],
+    filter: 'all'
+  }
+});
 
 const App = () => (
   <MuiThemeProvider muiTheme={getMuiTheme()}>
-      <div>
         <Header />
-        <TransferApp transfers={ transfers } />
-      </div>
   </MuiThemeProvider>
 );
+
+//  <TransferApp transfers={ transfers } />
 
 ReactDOM.render(
   <App />,
